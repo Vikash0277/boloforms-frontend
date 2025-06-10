2e79b28443ffdda0383971f12efc5d76dd861314
Vikash0277 <prasadvikash308@gmail.com>
2025-06-10 22:01:03 +0530

    backend url

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  'http://localhost:3000/api/v1',
+  baseURL:  'https://boloforms-backend.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
